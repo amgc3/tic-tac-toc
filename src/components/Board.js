6ff17b2faf6b58237e4927fcc1c4f622a1f00a82
Anna <adropofwater0@gmail.com>
2021-05-14 13:12:49 +0100

    board displaying styled

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,29 @@
-function Board() {
+import Square from './Square';
+
+const style = {
+    border: '4px solid green',
+    borderRadius: '5px',
+    width: '250ps',
+    heigth: '250px',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+};
+
+// style prop is default in react
+function Board(props) {
+    const {squares, onClick} = props;
     return (
-        <h1>Board</h1>
+        <div style={style}>
+            <Square value="1" onClick={ () => onClick("some value")}/>
+            <Square value="2" onClick={ () => onClick("some value")}/>
+            <Square value="3" onClick={ () => onClick("some value")}/>
+            <Square value="4" onClick={ () => onClick("some value")}/>
+            <Square value="5" onClick={ () => onClick("some value")}/>
+            <Square value="6" onClick={ () => onClick("some value")}/>
+            <Square value="7" onClick={ () => onClick("some value")}/>
+            <Square value="8" onClick={ () => onClick("some value")}/>
+            <Square value="9" onClick={ () => onClick("some value")}/>
+        </div>
     )
 }
 
